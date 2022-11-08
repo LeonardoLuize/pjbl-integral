694e86efcab019083b8fc5e4af97f08bc91f998b
LeonardoLuize <leonardo.luize2@gmail.com>
2022-11-08 17:52:22 -0300

    feat: add cors

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 from app.calculator.router import router as calculator_router
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:5173"
+]
 
 def create_app():
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(calculator_router)
+
 
     return app
 
